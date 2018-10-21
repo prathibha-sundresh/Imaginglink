1de40d86541bbdf45ddb5476603f1e1befbc0a4f
Imaginglink Inc <imaginglink@Imaginglinks-MacBook-Pro.local>
2018-10-21 19:54:54 +0530

    Added invite and change email screen

---
+++ ImaginingLink/VerificationView/ChangeEmailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ChangeEmailViewController.swift
+//  ImaginingLink
+//
+//  Created by Imaginglink Inc on 10/17/18.
+//  Copyright © 2018 Imaginglink Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ChangeEmailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
---
+++ ImaginingLink/VerificationView/InviteFriendsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  ImaginingLink
+//
+//  Created by Imaginglink Inc on 10/18/18.
+//  Copyright © 2018 Imaginglink Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class  InviteFriendsViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
