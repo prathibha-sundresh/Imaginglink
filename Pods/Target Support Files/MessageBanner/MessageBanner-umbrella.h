c0052b68c38112cf4d596071bf7662e00b279a4c
ramugprs <ramugps@gmail.com>
2020-03-30 12:53:07 +0530

    podlock file changed updated

---
+++ Pods/Target Support Files/MessageBanner/MessageBanner-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "MBLMessageBanner.h"
+#import "MBLMessageBannerView.h"
+
+FOUNDATION_EXPORT double MessageBannerVersionNumber;
+FOUNDATION_EXPORT const unsigned char MessageBannerVersionString[];
+
--- Pods/Target Support Files/SaltsideAPI/SaltsideAPI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SaltsideAPI : NSObject
-@end
-@implementation PodsDummy_SaltsideAPI
-@end
